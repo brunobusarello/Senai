fd067143b2a13abc83b2c1f8a58fcc3069d664f2
Bruno Gustavo Busarello <150341182+brunobusarello@users.noreply.github.com>
2024-07-01 15:52:42 -0300

    update calculadora.java

--- java/Java Swing/AppCalc/src/appcalc/Calculadora.java
+++ java/Java Swing/AppCalc/src/appcalc/Calculadora.java
@@ -42,7 +42,7 @@ public class Calculadora {
     
     // verifica se a expressão está balanceada (OK)
     public boolean expOk(String str){
-        /*
+        
         Pilha p = new Pilha();
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == '[' || str.charAt(i) == '(') {
@@ -64,8 +64,8 @@ public class Calculadora {
             ok = false;
         }
         return ok;
-        */
         
+        /*
         int contPar = 0;
         int contCol = 0;
         for (int i = 0; i < str.length(); i++) {
@@ -83,6 +83,9 @@ public class Calculadora {
                     contCol--;
                     break;
             }
+            if (contCol < 0 || contPar < 0) {
+                return false;
+            }
         }
         if (contCol == 0 && contPar == 0) {
             return true;
@@ -90,7 +93,7 @@ public class Calculadora {
         else {
             return false;
         }
-        
+        */
     }
     
     public String[] decompor(String str){
