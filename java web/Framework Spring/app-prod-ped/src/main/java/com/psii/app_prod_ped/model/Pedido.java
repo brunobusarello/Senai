48881fb988647fc8ceaecfd6d4ac4d673970994b
BRUNO GUSTAVO BUSARELLO <bruno_busarello@estudante.sesisenai.org.br>
2024-10-16 16:45:12 -0300

    New model from spring boot

---
+++ java web/Framework Spring/app-prod-ped/src/main/java/com/psii/app_prod_ped/model/Pedido.java
@@ -0,0 +1,49 @@
+package com.psii.app_prod_ped.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    Date dataPedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+
+    private Produto produto;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(Date dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+}
---
+++ java web/Framework Spring/app-prod-ped/src/main/java/com/psii/app_prod_ped/model/Produto.java
@@ -0,0 +1,40 @@
+package com.psii.app_prod_ped.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String descricao;
+    Double preco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+}
